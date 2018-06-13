98f3c8b547d847e5117dfdad67385fda49810ec5
insanemax <tm_4@mail.ru>
2018-06-13 11:28:59 +0300

    эта версия для изменений

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,8 @@ var sass = require("gulp-sass");
 var wait = require("gulp-wait");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
 var autoprefixer = require("autoprefixer");
-var minify = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var del = require("del");
 var server = require("browser-sync").create();
-var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -23,16 +16,13 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", function() {
+gulp.task("serve", ["style"], function() {
   server.init({
-    server: "build/",
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
@@ -41,47 +31,4 @@ gulp.task("serve", function() {
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
-});
-
-gulp.task("html", function() {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-        include()
-      ]))
-    .pipe(gulp.dest("build"));
-})
-
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png, jpg, svg}")
-    .pipe(imagemin([
-        imagemin.optipng({optimizationLevel: 3}),
-        imagemin.jpegtran({progressive: true}),
-        imagemin.svgo()
-      ]))
-    .pipe(gulp.dest("source/img"));
-});
-
-gulp.task("copy", function (){
-  return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
-    "source/img/**",
-    "source/js/**"
-    ], {
-      base: "source"
-    })
-  .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function() {
-  return del("build");
-});
-
-gulp.task("build", function(done) {
-  run(
-    "clean",
-    "copy",
-    "style",
-    "html",
-    done
-    );
 });
